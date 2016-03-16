9c18deea7466c74255eb482277d34ac55a05ad12
cemeteryum <cemeteryum@yahoo.com>
2016-03-16 10:16:52 +0000

    Merge with raku branch

---
+++ app/assets/javascripts/colorPallete.js
@@ -0,0 +1,43 @@
+function rdmColor(i){
+    
+  switch(i) {
+    case 1:
+    case 2:  return "31dd41"; 
+    case 3:  return "cf2cde"; 
+    case 4:
+    case 5:  return "fa0000";
+    case 6:
+    case 7:  return "754cae";
+    case 8:  return "7f7f7f"; 
+    case 9:
+    case 10:
+    case 11: return "dec02c";  
+    case 12: 
+    case 13:
+    case 14: return "de772c";
+    case 15: return "754cae";
+    case 16:
+    case 17: return "80de2c"; 
+    case 18: return "de772c";
+    case 19: return "662cde";
+    case 20: return "2c99de";
+    case 21:
+    case 22: return "cf2cde"; 
+    case 23: return "2cde90"; 
+    case 24:
+    case 25: 
+    case 26: return "662cde"; 
+    case 27:
+    case 28:
+    case 29: return "cf2cde";  
+    case 30:
+    case 31:
+    case 32: return "de2c90"; 
+    case 33:
+    case 34:
+    case 35: return "754cae"; 
+
+    default:        
+        return "7f7f7f";
+    }
+}
